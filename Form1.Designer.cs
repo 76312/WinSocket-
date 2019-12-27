386f06305229d4fdab954e0c3821a844b9b5d8b3
76312 <53069964+76312@users.noreply.github.com>
2019-12-27 13:17:48 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinSocket通信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtRec = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始监听";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtRec
+            // 
+            this.txtRec.Location = new System.Drawing.Point(26, 50);
+            this.txtRec.Multiline = true;
+            this.txtRec.Name = "txtRec";
+            this.txtRec.Size = new System.Drawing.Size(290, 220);
+            this.txtRec.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(74, 16);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(146, 21);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "5000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "端口";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 282);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtRec);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "服务器端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtRec;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WinSocket通信
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint port = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+            socketWatch.Bind(port);
+            ShowMsg("监听开始...");
+            socketWatch.Listen(10);
+            Thread th = new Thread(Listen);
+            th.IsBackground = true;
+            th.Start(socketWatch);
+        }
+
+        void Listen(object c)
+        {
+            Socket socketWatch = c as Socket;
+            while (true) 
+            {
+                Socket socketSend = socketWatch.Accept();
+                ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+                Thread th = new Thread(receiveData);
+                th.IsBackground = true;
+                th.Start(socketSend);
+            }
+        }
+
+        void receiveData(object c)
+        {
+            Socket socketSend = c as Socket;
+            while(true)
+            {
+                byte[] buffer = new byte[1024 * 1024 * 2];
+                int r = socketSend.Receive(buffer);
+                if (r == 0) break;
+                string str = Encoding.UTF8.GetString(buffer,0,r);
+                ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + str);
+            }
+        }
+
+        void ShowMsg(string str)
+        {
+            txtRec.AppendText(str + "\r\n");
+        }
+    }
+}
